61d5d3500390daf612d73b7804cb53328eca0469
bachmeyer <wesyb@yahoo.com>
2016-01-22 13:02:49 -0600

    basics of data types in c#

---
+++ csharpvariablesanddatatypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpvariablesanddatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20; //declaring a variable and assigning 20 to it.
+            Console.WriteLine("You are " + age + " years old.");
+            Console.ReadLine();
+
+        }
+    }
+}
